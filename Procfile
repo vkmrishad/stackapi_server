612c9816451785fdb5b16634369c75129e52bec8
Mohammed Rishad <rishad@reckonsys.com>
2019-12-25 16:45:14 +0530

    Files updated

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn stack_search.wsgi --log-file -
--- stack_search/settings.py
+++ stack_search/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 
@@ -165,3 +166,6 @@ CORS_ORIGIN_WHITELIST = (
     'localhost',
     '127.0.0.1'
 )
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
